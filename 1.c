3663c9a522315cfbe7351f862ea9c6ce5da00559
mayankmethad <mayankmethad@gmail.com>
2017-05-28 10:13:09 +0530

    Initial Pull

--- 1.c
+++ 1.c
@@ -1,56 +1,23 @@
 #define _POSIX_SOURCE
 #define _POSIX_C_SOURCE 199309L
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
+#include<stdio.h>
+#include<unistd.h>
+int main() {
 	long res;
-	
-	if((res = sysconf(_SC_CLK_TCK)) == -1)
-	{
-		printf("Error in sysconf\n");
-	}
-	else
-	{
-		printf("Number of clock ticks are = %d \n",res);
-	}
-
-	if((res = sysconf(_SC_CHILD_MAX)) == -1)
-	{
-		printf("Error in sysconf\n");
-	}
-	else
-	{
-		printf("Maximum number of child processes owned by a process are = %d \n",res);
-	}
-
-	if((res = pathconf("/",_PC_PATH_MAX)) == -1)
-	{
-		printf("Error in pathconf \n");
-	}
-	else
-	{
-		printf("Maximum path length is = %d \n",res);
-	}
-
-	if((res = pathconf("1.c",_PC_NAME_MAX)) == -1)
-	{
-		printf("Error in pathconf \n");
-	}
-	else
-	{
-		printf("Maximum number of characters in a file are = %d \n",res);
-	}
-
-	if((res = sysconf(_SC_OPEN_MAX)) == -1)
-	{
-		printf("Error in sysconf \n");
-	}
-	else
-	{
-		printf("Maximum number of open files are = %d \n",res);
-	}
-
+	if((res=sysconf(_SC_CLK_TCK))==-1)
+		printf("Error in _SC_CLK_TCK\n");
+	else printf("No of clock ticks=%ld\n",res);
+	if((res=sysconf(_SC_CHILD_MAX))==-1)
+		printf("Error in _SC_CHILD_MAX\n");
+	else printf("Max no of child process=%ld\n",res);
+	if((res=pathconf("/",_PC_PATH_MAX))==-1)
+		printf("Error in _PC_CHILD_MAX\n");
+	else printf("Max path length=%ld\n",res);
+	if((res=pathconf("1.c",_PC_NAME_MAX))==-1)
+		printf("Error in _PC_NAME_MAX\n");
+	else printf("Max no of character in a file name=%ld\n",res);
+	if((res=sysconf(_SC_OPEN_MAX))==-1)
+		printf("Error in _SC_OPEN_MAX\n");
+	else printf("Max no of open files per process=%ld\n",res);
 	return 0;
 }
--- 2.c
+++ 2.c
@@ -1,39 +1,36 @@
 #define _POSIX_SOURCE
 #define _POSIX_C_SOURCE 199309L
-#include <stdio.h>
-#include <unistd.h>
-
-int main()
-{
+#include<stdio.h>
+#include<unistd.h>
+int main() {
 	#ifdef _POSIX_JOB_CONTROL
-		printf("System supports job control \n");
+		printf("System supports job control=%d\n",_POSIX_JOB_CONTROL);
 	#else
-		printf("System doesn't support job control \n")
-	#endif
-
-	#ifdef _POSIX_NO_TRUNC
-		printf("System supports path trunc option \n");
-	#else
-		printf("System doesn't support path trunc \n");
+		printf("System doesn't support job control\n");
 	#endif
 
 	#ifdef _POSIX_SAVED_IDS
-		printf("System supports saved set UID and GID \n");
+		printf("System supports saved set-uid and set-gid=%d\n",_POSIX_SAVED_IDS);
 	#else
-		printf("System doesn't support saved set IDs \n");
+		printf("System doesn't supports saved set-uid and set-gid\n");
 	#endif
 
-	#ifdef _POSIX_CHOWN_RESTRICTED	
-		printf("System supports owner change in files \n");
+	#ifdef _POSIX_CHOWN_RESTRICTED
+		printf("System supports change ownership permission=%d\n",_POSIX_CHOWN_RESTRICTED);
 	#else
-		printf("System doesn't support owner change of files \n");
+		printf("System doesn't supports change ownership permission\n");
 	#endif
-
+	
 	#ifdef _POSIX_VDISABLE
-		printf("System supports VDISBLE \n");
+		printf("System supports disabling special characters=%d\n",_POSIX_VDISABLE);
 	#else
-		printf("System doesn't support VDISABLE \n");
+		printf("System doesn't support disabling special characters\n");
 	#endif
 
+	#ifdef _POSIX_NO_TRUNC
+		printf("System supports pathname trunc options=%d\n",_POSIX_NO_TRUNC);
+	#else
+		printf("System doesn't support pathname trunc option\n");
+	#endif
 	return 0;
 }
