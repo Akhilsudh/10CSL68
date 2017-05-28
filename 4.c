8f509cd741238252ca1639036bab56f63ac11d1f
Mayank Metha D <mayankmethad@gmail.com>
2017-05-28 09:42:50 +0530

    Optional changes to 4.c and list of headers used

--- 4.c
+++ 4.c
@@ -1,33 +1,26 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-
-int main(int argc, char* argv[])
-{
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+int main(int argc,char **argv) {
 	int fd;
-	char buff[256];
-	if(argc != 2 && argc !=3)
-	{
-		printf("Invalid number of arguements\n");
-		return 0;
+	char buf[256];
+	if(argc!=2&&argc!=3) {
+		printf("Usage error\n");
+		return 1;
 	}
-	mkfifo(argv[1], S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
-	if(argc == 2)
-	{
-		fd = open(argv[1], O_RDONLY);
-		printf("Contents of the FIFO file are \n");
-		while(read(fd, buff, sizeof(buff)) > 0)
-		{
-			printf("%s\n",buff);
-		}
-	}
-	else
-	{
-		fd = open(argv[1], O_WRONLY);
-		write(fd, argv[2], strlen(argv[2]));
+	//mfifo(argv[1],S_IFIFO|S_IRWXU|S_IRWXG|S_IRWXO);
+	mkfifo(argv[1],S_IFIFO|0777);
+	if(argc==2) {
+		fd=open(argv[1],O_RDONLY);
+		printf("\nContent of fifo are:\n");
+		while(read(fd,buf,sizeof(buf))>0)
+			printf("%s\n",buf);
+	} else {
+		fd=open(argv[1],O_WRONLY);
+		write(fd,argv[2],strlen(argv[2]));
 	}
 	close(fd);
 }
