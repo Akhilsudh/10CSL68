9b2060b59682bee7e737117f23a2d5baae320e9f
Akhil S <akhil.sudh@gmail.com>
2017-05-28 11:42:25 +0530

    Added 5b.c

---
+++ 5b.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) 
+{
+	if(argc == 3)
+		if(link(argv[1], argv[2]) == -1)
+			printf("Errror found\n");
+		else
+			printf("New hard link created\n");
+	else
+		printf("Insufficient arguements\n");
+	return 0;
+}
